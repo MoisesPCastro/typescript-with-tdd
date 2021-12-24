fe5f2491511775076c0ff090776b707408bd10fd
Moises Pires <moisespires1@Hotmail.com>
2021-12-24 20:34:40 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup';
+
+describe('SignUp Contoller', () => {
+    test('Should return 400 if no name is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+            email: 'moi_teste@teste.com',
+            password: '1234',
+            passwordConfirmation: '1234'
+        }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    })
+})
