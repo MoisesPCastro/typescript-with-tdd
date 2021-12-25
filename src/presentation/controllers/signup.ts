4ca8fe4de4ed9178b994287f72371450cd55192b
Moises Pires <moisespires1@Hotmail.com>
2021-12-25 10:32:45 -0300

    feat: ensure SignUpController returns 400 if no passord is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,7 +5,7 @@ import { HttpRequest, HttpResponse } from './../protocols/https';
 
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFilds = ['name', 'email']
+        const requiredFilds = ['name', 'email', 'password', 'passwordConfirmation']
 
         for (const field of requiredFilds) {
             if (!httpRequest.body[field]) {
