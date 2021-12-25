b2f84cfcea1f966d071fff7b7a9f2b9ff4c27d38
Moises Pires <moisespires1@Hotmail.com>
2021-12-25 09:24:27 -0300

    test: ensure SignUpController return erro if email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,14 +5,29 @@ describe('SignUp Contoller', () => {
         const sut = new SignUpController()
         const httpRequest = {
             body: {
-            email: 'moi_teste@teste.com',
-            password: '1234',
-            passwordConfirmation: '1234'
+            email: 'any_email',
+            password: 'any_password',
+            passwordConfirmation: 'any_password'
         }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    
+    }) 
+
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+            name: 'any_name',
+            password: 'any_password',
+            passwordConfirmation: 'any_password',
+        }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
     
     }) 
     
