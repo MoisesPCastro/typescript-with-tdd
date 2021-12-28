664e7ad8fced3976a917e2eae45b321b64f47eae
Moises Pires <moisespires1@Hotmail.com>
2021-12-28 16:32:38 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -188,4 +188,45 @@ describe('SignUp Contoller', () => {
             password: 'any_password',
         })
     })
+
+    test('Should return 500 if AddAccount throws', () => {
+
+        const {sut, addAccountStub} = makeSut()
+        jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+            throw Error()
+        })
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'invalid_email@teste.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
+
+    })
+
+    test('Should return 200 if valid data is providad', () => {
+        const {sut} = makeSut()
+        const httpRequest = {
+            body: {
+                name: 'valid_name',
+                email: 'valid_email@teste.com',
+                password: 'valid_password',
+                passwordConfirmation: 'valid_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
+        })
+
+    })
 })
