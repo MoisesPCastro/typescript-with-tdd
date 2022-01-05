41ec3d12fca31d5c399a659ce6c7354fb33a9f17
Moises Pires <moisespires1@Hotmail.com>
2022-01-04 22:06:25 -0300

    feat: encure DbAccount calls AddAccountRepository with corrent values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,19 +1,20 @@
-import { Encrypter } from './../../protocols/encrypter';
-import { AccountModel } from './../../../domain/models/account';
-import { AddAccount, AddAccountModel } from './../../../domain/usecases/add-account';
+import { AddAccount, AddAccountModel, AccountModel,  Encrypter, AddAccountRepository} from './db-add-account-protocol';
 
 
 export class DbAddAccount implements AddAccount{
     private readonly encrypter: Encrypter
+    private readonly addAccountRepository: AddAccountRepository
 
-    constructor(encrypter: Encrypter){
+    constructor(encrypter: Encrypter, addAccountRepository: AddAccountRepository ){
         this.encrypter = encrypter
+        this.addAccountRepository = addAccountRepository
     }
 
-   async add(account: AddAccountModel): Promise<AccountModel>{
+   async add(accountData: AddAccountModel): Promise<AccountModel>{
 
-    await this.encrypter.encrypt(account.password)
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    const result = await this.addAccountRepository.add(Object.assign({}, accountData, {password: hashedPassword}))
 
-        return new Promise(resolve => resolve(null))
+        return new Promise(resolve => resolve(result))
     }
 }
