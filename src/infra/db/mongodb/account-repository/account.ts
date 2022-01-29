14a657ac0183167979d2d72a7ac0a4d4cebc9afd
Moises Pires <moisespires1@Hotmail.com>
2022-01-29 09:58:20 -0300

    feat: com erro no account.ts

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+
+import { MongoHelper } from './helpes/mongo-helpes';
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+
+
+export class AccountMongoRepository implements AddAccountRepository {
+   async add (accountData: AddAccountModel): Promise <AccountModel> {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        const result = await accountCollection.insertOne(accountData)
+
+        return MongoHelper.map(result[0])
+      
+    }
+}
