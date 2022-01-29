45134d6f1308d1b34e2ad1883884985eb2465c57
Moises Pires <moisespires1@Hotmail.com>
2022-01-29 16:41:38 -0300

    refactor: connect mongo

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from './../../infra/db/mongodb/account-repository/helpes/mongo-helpes';
+import request from "supertest"
+import app from "../config/app"
+
+beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+ })
+
+ afterAll( async () => {
+    await MongoHelper.disconnect()
+ })
+
+ beforeEach(async () => {
+     const accountCollection = MongoHelper.getCollection('accounts')
+     await accountCollection.deleteMany({})
+ })
+
+describe('SignUp Routes', () => {
+    test('Shold return an account on success', async() => {
+
+        await request(app)
+            .post('/api/signup')
+            .send({
+                name: 'moises',
+                email: 'moisesteste@test.com',
+                password: '123',
+                passwordConfirmation: '123'
+            })
+            .expect(200)
+    })
+}) 
